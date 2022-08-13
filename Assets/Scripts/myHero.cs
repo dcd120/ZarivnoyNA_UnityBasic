f3c2ddfbf31435eb006e2e5c5e195a60bbf19fec
Nikolay <dcd120@mail.ru>
2022-08-14 00:04:40 +0300

    HW 7
    добавлены:
    1. Слежение за курсором мыши
    2. Перемещение персонажа стрейфом
    3. Меню
    4. Возможность загрузки нескольких уровней (через меню)

--- Assets/Scripts/myHero.cs
+++ Assets/Scripts/myHero.cs
@@ -9,6 +9,7 @@ namespace EI2
     public class myHero : MonoBehaviour
     {
         private Vector3 m_Movement;
+        private Vector3 m_LookUp;
         private Animator m_Animator;
         private Rigidbody m_Rigidbody;
         private AudioSource m_FootSteps;
@@ -24,6 +25,7 @@ namespace EI2
         [SerializeField] public string m_HeroName = "Leonard";
         [SerializeField] public string m_groundTag = "Titles";
         [SerializeField] public TMP_Text m_HeroInfoUI;
+        [SerializeField] public Transform m_Pointer;
 
         /// <summary>
         /// Время "остывания" метательного оружия героя
@@ -60,11 +62,11 @@ namespace EI2
         void Update()
         {
             // проверка, что будет, если в апдейт добавить while
-            int i = 0;
-            while (i < 100)
-            {
-                i++;
-            }
+            //int i = 0;
+            //while (i < 100)
+            //{
+            //    i++;
+            //}
 
             UpdateInfo();
 
@@ -74,7 +76,16 @@ namespace EI2
             float jump = Input.GetAxis("Jump");
 
             m_Movement.Set(horizontal, 0f, vertical);
+            Debug.DrawRay(transform.position, m_Movement * 5, Color.red);
+            m_Movement += transform.forward * 0.001f;
+            // надо скорректировать относительно поворота персонажа!
+            Debug.Log($"horizontal: {horizontal} / vertical: {vertical}");
+
+            Debug.DrawRay(transform.position, m_Movement * 3, Color.blue);
+            
             m_Movement.Normalize();
+
+            Debug.DrawRay(transform.position, m_Movement, Color.yellow, 0.5f);
 
             bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
             bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
@@ -96,7 +107,27 @@ namespace EI2
                 m_FootSteps.Stop();
             }
 
-            Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+            //Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+            //m_Rotation = Quaternion.LookRotation(desiredForward);
+            // сделаем слежение за мышкой
+            //m_Movement.Set(Input.mousePosition.x, 0f, Input.mousePosition.z);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //Debug.DrawRay(transform.position, Input.mousePosition, Color.blue);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                m_LookUp = hit.point;
+                m_LookUp.y = transform.up.y;
+                m_Pointer.position = m_LookUp;
+            }
+            //Debug.DrawRay(m_Rigidbody.position, m_LookUp, Color.red);
+            //Debug.DrawRay(m_Rigidbody.position, transform.forward * 5, Color.blue);
+
+            m_LookUp = m_LookUp - m_Rigidbody.position;
+            //Debug.DrawRay(m_Rigidbody.position, m_LookUp, Color.green, 0.5f);
+
+            Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_LookUp, turnSpeed * Time.deltaTime, 0f);
+            //Debug.DrawRay(transform.position, desiredForward, Color.magenta, 1f, false);
+
             m_Rotation = Quaternion.LookRotation(desiredForward);
 
             if ((is_jump) && (isGrounded))
@@ -115,6 +146,7 @@ namespace EI2
 
             if (Input.GetButtonDown("Fire1") && (readyToFire))
             {
+                //Debug.Log($"m_LookUp: {m_LookUp} / Hero forward: {transform.forward} / desiredForward: {desiredForward}");
                 Instantiate(fire, m_Rigidbody.position + transform.forward + transform.up, m_Rigidbody.rotation);
                 readyToFire = false;
                 timeToRechargeFire = Time.fixedTime;
@@ -124,6 +156,8 @@ namespace EI2
         }
         void OnAnimatorMove()
         {
+            //Debug.DrawRay(m_Rigidbody.position, m_Rigidbody.position + m_Movement, Color.black);
+            
             m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
             m_Rigidbody.MoveRotation(m_Rotation);
             //Debug.Log($"Hero rot:{m_Rotation}");
